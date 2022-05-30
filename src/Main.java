15d15142c4e7f7899f5a2964796537e016fd58fe
KREnZE1 <r.henze06@web.de>
2022-05-30 12:26:15 +0200

    Struktur erledigt

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,46 @@
 public class Main {
+    static double[][] werte;
+    static int[][] gehasht;
+    static int modulo;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    }
+
+    public static int berechneHash(int pSchluessel) {
+        //TODO: Methode schreiben
+        return 0;
+    }
+
+    public static int fuegeEin(int pSchluessel, double wert) {
+        //TODO: Methode schreiben
+        //In Geburtswert Tabelle einfuegen
+        return 0;
+    }
+
+    public static int suchePositionVon(int pSchluessel) {
+        //TODO: Methode schreiben
+        return 0;
+    }
+
+    public static int fuegeEin(int pSchluessel) {
+        //TODO: Methode schreiben
+        //In Gehashte Tabelle einfuegen
+        return 0;
+    }
+
+    public static double[][] gibWerte() {
+        //TODO: Methode schreiben
+        return new double[3][];
+    }
+
+    public static int[][] gibGehasht(){
+        //TODO: Methode schreiben
+        return new int[3][];
+    }
+
+    public static double gibWertVonGeburtsdatum(int pGeburtsdatum) {
+        //TODO: Methode schreiben
+        return 0;
     }
 }
