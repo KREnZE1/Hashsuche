d5fabf8ac906cdb1dc7d0b589296cf0d65629f27
KREnZE1 <r.henze06@web.de>
2022-06-02 10:58:35 +0200

    Rückgang zum eindimensionalen Array

--- src/Hashsuche.java
+++ src/Hashsuche.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class Hashsuche {
     int modulo;
-    static double[][] geburtsdaten;
+    static double[] geburtsdaten;
     static int[] hashTabelle;
     static Hashsuche test;
 
@@ -16,7 +16,7 @@ public class Hashsuche {
 
     public static void setup(){
         test = new Hashsuche(11);
-        geburtsdaten = new double[20][2];
+        geburtsdaten = new double[test.getModulo()];
         hashTabelle = new int[test.getModulo()];
     }
 
@@ -25,43 +25,35 @@ public class Hashsuche {
         }
         public int getModulo() {return this.modulo;}
 
-    public static void hashEinfuegen(int pSchluessel) {
-        //hashTabelle[berechneHash(pSchluessel)] = pSchluessel;
+    public static int hashEinfuegen(int pSchluessel) {
         int hash = berechneHash(pSchluessel);
-        rekEinfuegen(hash, pSchluessel, 0);
+        return rekEinfuegen(hash, pSchluessel, 0);
     }
 
-    public static void rekEinfuegen(int index, int pSchluessel, int kollisionen) {
-        if (kollisionen > hashTabelle.length) return;
-        if (index > hashTabelle.length) index -= kollisionen;
+    public static int rekEinfuegen(int index, int pSchluessel, int kollisionen) {
+        if (kollisionen > hashTabelle.length) return -1;
+        if (index > hashTabelle.length) index -= hashTabelle.length;
         if (hashTabelle[index] == 0) {
             hashTabelle[index] = pSchluessel;
-
-            //noinspection UnnecessaryReturnStatement
-            return;
+            return index;
         }
-        else rekEinfuegen(index+1, pSchluessel, kollisionen+1);
+        else return rekEinfuegen(index+1, pSchluessel, kollisionen+1);
     }
-    public static int berechneHash(int pSchluessel) {
-        return pSchluessel%test.getModulo();
-    }
+    public static int berechneHash(int pSchluessel) {return pSchluessel%test.getModulo();}
 
     public static void geburtswertEinfuegen(int pGeburtswert, double pValue) {
-        for (double[] value : geburtsdaten) {
-            if (value[0] == 0) {
-                value[0] = pGeburtswert;
-                value[1] = pValue;
-                hashEinfuegen((int) value[0]);
-                return;
-            }
-        }
+        geburtsdaten[hashEinfuegen(pGeburtswert)] = pValue;
     }
 
-    public static void ausgabeGeburtswerte() {
-        for (double[] values : geburtsdaten) System.out.print(Arrays.toString(values)+"\n");
+    public static void ausgabeGeburtswerte() {System.out.print(Arrays.toString(geburtsdaten)+"\n");
     }
 
-    public static void ausgabeHashTabelle() {
-        System.out.print(Arrays.toString(hashTabelle)+"\n");
+    public static void ausgabeHashTabelle() {System.out.print(Arrays.toString(hashTabelle)+"\n");}
+
+    public static int suchePositionVon(int pGeburtswert) {
+        //Eingabe ein Geburtswert, Ausgabe der index in der hashtabelle (was der index in der normalen tabelle ist)
+
+        return -1;
+        //TODO: Suche implementieren
     }
 }
